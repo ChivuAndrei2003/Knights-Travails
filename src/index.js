1f3f7f2a3c7dca7eab68cd660852873142601f2b
Chivu Andrei <chivuandrei2003@gmail.com>
2025-08-30 18:27:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-let a;
+let chessBoard = [
+
+];
+function knightMoves(x = [], y = []) {
+    let chessBoard = [];
+    chessBoard.fill()
+   
+    
+}
+knightMoves();
+
+//trebuie sa folosesc djikstra
+let knightAllowedSquares = [
+    
+]
